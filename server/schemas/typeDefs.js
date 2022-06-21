2d81734917ec353dc2cf1f8ffec6e074cb9ab291
pixiepucker <autumnoffic@gmail.com>
2022-06-21 14:07:26 -0500

    updated typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,26 +3,27 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type Auth {
-    token: Int
-}
-type Book {
+  type Book {
     bookId: ID
     authors: [String]
     description: String
     title: String
     image: String
     link: String
-}
-type User {
+  }
+
+  type User {
     -id: ID
     username: String
     email: String
     bookCount: Int,
     savedBooks: [Book]
-}
+  }
+
   type Query {
     me: User
+    removeBook(bookId: ID!): User
+    books: [Book]
   }
 `;
 
