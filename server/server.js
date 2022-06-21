e0353ba57412dd2ed48f8043070f46c806087bff
pixiepucker <autumnoffic@gmail.com>
2022-06-21 13:08:47 -0500

    updated server.js w Apollo

--- server/server.js
+++ server/server.js
@@ -1,21 +1,46 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+// import typeDefs and resolvers
+const { typeDefs, resovlers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+// new ApolloServer
+const server = new ApolloServer({
+  typeDefs,
+  resovlers,
+});
+
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+const startApolloServer = async (typeDefs, resovlers) => {
+  await server.start();
 
-app.use(routes);
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () =>
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    );
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
