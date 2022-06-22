323234bd5f1a58054e1afdaa7a0e435bc9e1e6b2
pixiepucker <autumnoffic@gmail.com>
2022-06-22 14:47:48 -0500

    added Get_Me query and addUser/Login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,32 @@
 const { Book, User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello world!';
+    me: async () => {
+      return User.find().select('-__v -password').populate('books');
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return user;
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect email!');
+      }
+
+      const correctPW = await user.isCorrectPassword(password);
+
+      if (!correctPW) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      return user;
     },
   },
 };
